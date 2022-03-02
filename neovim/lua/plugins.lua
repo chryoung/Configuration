50acda42cecec1b60241152f95c22271469936a7
Ewan Redfield <29620734+chryoung@users.noreply.github.com>
2022-03-02 23:17:50 +0800

    Add which-key for help

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -6,23 +6,23 @@
 return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-  
+
     -- Close the parenthesis
     use '9mm/vim-closer'
-  
+
     -- Dispatch job asynchronously
     use { 'tpope/vim-dispatch', opt = true, cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } }
-  
+
     -- Matches the parenthesis and more
     use { 'andymass/vim-matchup', event = 'VimEnter' }
-  
+
     -- Markdown support
     use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview' }
     use { 'preservim/vim-markdown', requires = { 'godlygeek/tabular' } }
-  
+
     -- Themes
     use { 'dracula/vim', as = 'dracula' }
-  
+
     -- File management
     use {
       'kyazdani42/nvim-tree.lua',
@@ -34,13 +34,13 @@ return require('packer').startup(function()
 
     -- Git support
     use 'tpope/vim-fugitive'
-  
+
     -- File search
     use { 'Yggdroot/LeaderF', run = ':LeaderfInstallCExtension' }
-  
+
     -- Commenet code. Type gcc to comment a line
     use { 'tpope/vim-commentary' }
-  
+
     -- Fast move
     use {
       'phaazon/hop.nvim',
@@ -50,30 +50,34 @@ return require('packer').startup(function()
         require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
       end
     }
-  
+
     -- Snippet
     use 'SirVer/ultisnips'
-  
+
     -- Status line
     use {
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true },
       config = function() require('lualine').setup() end
     }
-  
+
     -- Command completion
     use { 'gelguy/wilder.nvim', run = ':UpdateRemotePlugins' }
-  
+
     -- Type :AsyncRun to run a command aschronously
     use { 'skywind3000/asyncrun.vim', cmd = {'AsyncRun'} }
-  
+
     -- Better highlighting
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  
+
     -- Smooth scroll
     use { 'karb94/neoscroll.nvim', config = function() require('neoscroll').setup() end }
-  
+
     use 'simnalamburt/vim-mundo'
-  
+
+    use {
+      "folke/which-key.nvim",
+      config = function() require("which-key").setup() end
+    }
+
 end)
-  
