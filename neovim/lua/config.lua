79276c9ba01071d519859001f4eff79d92d65301
Ewan Redfield <29620734+chryoung@users.noreply.github.com>
2022-04-23 12:22:34 +0800

    Search both ruby and rails doc for ruby file on Dash

--- neovim/lua/config.lua
+++ neovim/lua/config.lua
@@ -1,1 +1,13 @@
-require('telescope').load_extension('fzf')
+local telescope = require('telescope')
+telescope.load_extension('fzf')
+telescope.setup({
+  extensions = {
+    dash = {
+      search_engine = 'google',
+      file_type_keywords = {
+        ruby = { 'ruby', 'rails' },
+      }
+    }
+  }
+})
+
