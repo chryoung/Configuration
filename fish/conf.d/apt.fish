84f61eb494ed3940fdfe36fc734b8a025854f606
Ewan Redfield <29620734+chryoung@users.noreply.github.com>
2022-07-05 02:33:55 +0800

    Add fish configuration

---
+++ fish/conf.d/apt.fish
@@ -0,0 +1,4 @@
+alias ai "sudo apt install"
+alias adg "sudo apt-get update && sudo apt-get upgrade"
+alias acs "apt-cache search"
+
---
+++ fish/conf.d/common_alias.fish
@@ -0,0 +1,8 @@
+alias ll "ls -l"
+alias la "ls -la"
+alias l "ls -la"
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ..... "cd ../../../.."
+
